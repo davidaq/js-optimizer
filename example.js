28023a167721dbfed2469499f617b74965833103
DavidAQ <anqin@ccme.cc>
2015-09-25 18:52:05 +0800

    startup

---
+++ example.js
@@ -0,0 +1,64 @@
+//function hello(arg) {
+//    var c = {console:console,b:[1,2,3]};
+//    var arr = [1,2,c, {a:1}];
+//    console.log(arr[0+2]);
+//    (c.console)['log']('hi');
+//    var a = !1;
+//    var b;
+//    b = a + (function() {
+//        return 2;
+//    })();
+//}
+//(function MAIN() {
+//    hello();
+//    MAIN();
+//})();
+
+//var a = 1;
+//if(a > 5 && 2 < a - 3) {
+//    console.log('a');
+//} else if(a > 10) {
+//    console.log('x');
+//} else {
+//    console.log('c');
+//}
+
+//while(!(a > 5)) {
+//    1+(1==2);
+//}
+
+//for (var k = 0; k < arr.length; k++) {
+//    arr[k]();
+//}
+//for (var k in arr) {
+//    arr[k]();
+//}
+
+try {
+
+} catch(e) {
+
+} finally {
+    
+}
+
+a = new b();
+
+a = 1, b = 2, b()
+
+c = a > 2 ? a-1 : b;
+
+while(false) {
+    continue;
+}
+switch(a) {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    default:
+        break;
+}
+
